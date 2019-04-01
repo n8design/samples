7eb0f6e9a98bdb130bb6ac518fbc4e5165bff8df
Stefan Bauer <stefan.bauer@n8d.at>
2019-04-01 13:18:46 +0200

    UpUpdated pre-SEF

--- uifbeyond/src/webparts/theFlexBox/components/TheFlexBox.tsx
+++ uifbeyond/src/webparts/theFlexBox/components/TheFlexBox.tsx
@@ -7,17 +7,35 @@ export default class TheFlexBox extends React.Component<ITheFlexBoxProps, {}> {
   public render(): React.ReactElement<ITheFlexBoxProps> {
     return (
       <div className={ styles.theFlexBox }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
+        <div className="item-card">
+          <div className="item-text">A</div>
+        </div>
+        <div className="item-card">
+          <div className="item-text">B</div>
+        </div>
+        <div className="item-card">
+          <div className="item-text">C</div>
+        </div>
+        <div className="item-card">
+          <div className="item-text">D</div>
+        </div>
+        <div className="item-card">
+          <div className="item-text">E</div>
+        </div>
+        <div className="item-card">
+          <div className="item-text">F</div>
+        </div>
+        <div className="item-card">
+          <div className="item-text">G</div>
+        </div>
+        <div className="item-card">
+          <div className="item-text">H</div>
+        </div>
+        <div className="item-card">
+          <div className="item-text">I</div>
+        </div>
+        <div className="item-card">
+          <div className="item-text">J</div>
         </div>
       </div>
     );
---
+++ uifbeyond/src/webparts/theGrid/TheGridWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+
+import * as strings from 'TheGridWebPartStrings';
+import TheGrid from './components/TheGrid';
+import { ITheGridProps } from './components/ITheGridProps';
+
+export interface ITheGridWebPartProps {
+  description: string;
+}
+
+export default class TheGridWebPart extends BaseClientSideWebPart<ITheGridWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<ITheGridProps > = React.createElement(
+      TheGrid,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
