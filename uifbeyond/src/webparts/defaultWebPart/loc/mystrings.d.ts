f3b2cc41b7c7909a3d93927531bb75eeb132f486
Stefan Bauer <stefan.bauer@n8d.at>
2019-04-03 15:54:14 +0200

    Update Flexbox, Grid and Adaptive Web Part Samples

---
+++ uifbeyond/src/webparts/defaultWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDefaultWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DefaultWebPartWebPartStrings' {
+  const strings: IDefaultWebPartWebPartStrings;
+  export = strings;
+}
--- uifbeyond/src/webparts/theGrid/components/TheGrid.tsx
+++ uifbeyond/src/webparts/theGrid/components/TheGrid.tsx
@@ -7,46 +7,30 @@ export default class TheGrid extends React.Component<ITheGridProps, {}> {
   public render(): React.ReactElement<ITheGridProps> {
     return (
       <div className={ styles.theGrid }>
-        <div>1</div>
-        <div>2</div>
-        <div>
-          <div className="header">
-            ancdefg
-          </div>
-        </div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>8</div>
-        <div>9</div>
-        <div>10</div>
-        <div>11</div>
-        <div>12</div>
-        <div>13</div>
-        <div>14</div>
-        <div>15</div>
-        <div>16</div>
-        <div>17</div>
-        <div>18</div>
-        <div>19</div>
-        <div>20</div>
-        <div>21</div>
-        <div>22</div>
-        <div>23</div>
-        <div>24</div>
-        <div>25</div>
-        <div>26</div>
-        <div>27</div>
-        <div>28</div>
-        <div>29</div>
-        <div>30</div>
-        <div>31</div>
-        <div>32</div>
-        <div>33</div>
-        <div>34</div>
-        <div>35</div>
-        <div>36</div>
+        <div>Lorem, ipsum.</div>
+        <div>Cupiditate, quidem.</div>
+        <div>Expedita, a?</div>
+        <div>Nulla, quas?</div>
+        <div>Pariatur, cum!</div>
+        <div>Placeat, corporis?</div>
+        <div>Nulla, ducimus.</div>
+        <div>Dicta, amet?</div>
+        <div>Nulla, voluptate.</div>
+        <div>Dolore, doloremque?</div>
+        <div>Dicta, ad?</div>
+        <div>Itaque, tempore.</div>
+        <div>Enim, sapiente.</div>
+        <div>Quisquam, vero.</div>
+        <div>Ullam, iusto.</div>
+        <div>Officiis, quo?</div>
+        <div>Cum, fuga?</div>
+        <div>Suscipit, impedit?</div>
+        <div>Earum, voluptate.</div>
+        <div>Totam, dolorum!</div>
+        <div>Esse, assumenda?</div>
+        <div>Veniam, delectus.</div>
+        <div>Aperiam, ratione!</div>
+        <div>Beatae, nostrum.</div>
       </div>
     );
   }
